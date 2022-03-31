6dbee1439c9c9b5b5dbe9a0aabbabd3c95363c15
Abhay Kumar Aman <93065225+Abhay5991@users.noreply.github.com>
2022-03-31 14:25:38 +0530

    File Upload
    SMC projects

---
+++ StudentManagementSystem/Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using StudentManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    
+    public class LoginController : Controller
+    {
+        SMSMVCEntities db = new SMSMVCEntities();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(User objchk)
+        {
+            if (ModelState.IsValid)
+            {
+                using(SMSMVCEntities db = new SMSMVCEntities())
+                {
+                    var obj = db.Users.Where(a => a.username.Equals(objchk.username) && a.password.Equals(objchk.password)).FirstOrDefault();
+
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id.ToString();
+                        Session["UserName"] = obj.username.ToString();
+                        return RedirectToAction("Index", "Home");
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The UserName Or Password is incorrect");
+                    }
+                }
+            }
+
+            return View(objchk);
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+
+
+    }
+}
